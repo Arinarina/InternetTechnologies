d07ad436bed2330ea02f9d72cd7470aa46306304
Arinarina <55127257+Arinarina@users.noreply.github.com>
2021-12-20 19:27:28 +0300

    Create script.js

---
+++ VueJS/4/script.js
@@ -0,0 +1,10 @@
+Vue.createApp({
+    data() {
+        return {
+            currentTeg:'"Клик", и я поменяюсь',
+            currentTegFirst:'"Клик", и я поменяюсь',
+            isHidden: true
+        };
+    }
+}
+).mount('#app');
