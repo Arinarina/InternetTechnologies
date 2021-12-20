e18be4f2cbc49c7fe3163bd45a0b2ee3523800c7
Arinarina <55127257+Arinarina@users.noreply.github.com>
2021-12-20 19:07:21 +0300

    Create script.js

---
+++ VueJS/3/script.js
@@ -0,0 +1,35 @@
+Vue.createApp({
+    data() {
+        return {
+            search: '',
+            articles: [
+                { 
+                    article: 'Nissan',
+                    url:'https://www.nissan.ru/', 
+                    img:"https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/Nissan_2020_logo.svg/1024px-Nissan_2020_logo.svg.png"  
+                },
+                {
+                    article: 'Mazda',
+                    url:'https://www.mazda.ru/',
+                    img:'https://main-cdn.goods.ru/hlr-system/634/425/185/524/164/8/100025294718b0.jpg'  
+                },
+                 {
+                     article: 'Ford', 
+                     url:'https://www.ford.ru/', 
+                     img:'https://interior-stickers.ru/68628-thickbox_default/logotip-ford-ford.jpg'
+                 },
+                 {
+                     article: 'Totota', 
+                     url:'https://www.toyota.ru/', 
+                     img:'https://main-cdn.goods.ru/hlr-system/1571438726/600000374933b0.png'  
+                 }
+            ]
+        };
+    },
+    computed:{
+        filter_articles() {
+            return this.articles.filter(tmp => {
+                return tmp.article.toLowerCase().indexOf(this.search.toLowerCase()) !== -1;});
+        }
+    }
+}).mount('#app');
